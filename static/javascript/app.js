85ca783fc8ed4d91d91a61dedd1ef3e1fa449a5b
Daniel Bradley <87danielbradley@gmail.com>
2020-07-29 15:52:12 -0400

    update

--- static/javascript/app.js
+++ static/javascript/app.js
@@ -12,19 +12,29 @@ $(window).scroll(() => {
 var projectListOne = [
 	"<p>Mini Projects</p>",
 	"<hr>",
-	"<div><a href='https://fredlintz5.github.io/week-4-game/' target='_blank'>Star Wars RPG</a></div>",
-	"<div><a href='https://fredlintz5.github.io/rochambeau/' target='_blank'>Rochambeau</a></div>",	
-	"<div><a href='https://fredlintz5.github.io/triviaGame/' target='_blank'>Trivia Game</a></div>",
-	"<div><a href='https://fredlintz5.github.io/bicycle_rentals/' target='_blank'>Bicycle Rentals</a></div>",
+	"<div><a href='https://87danielbradley.github.io/javascript-challenge/' target='_blank'>Javascript Challenge</a></div>",
+	"<div><a href='https://87danielbradley.github.io/Plotly-Challenge/' target='_blank'>Plotly Challenge</a></div>",	
+	"<div><a href='https://87danielbradley.github.io/D3-Challenge/' target='_blank'>D3 Challenge</a></div>",
+	"<div><a href='https://87danielbradley.github.io/Leaflet-Challenge/' target='_blank'>Leaflet Challenge</a></div>",
+	"<div><a href='https://87danielbradley.github.io/Web-Design-Challenge/' target='_blank'>Web Design Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/pandas-challenge' target='_blank'>Pandas Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/python-api-challenge' target='_blank'>Python API Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/Matplotlib' target='_blank'>Matplotlib Challenge</a></div>"
+
 	];
 		
 var projectListTwo = [
 	"<p>Mini Projects</p>",
  	"<hr>",
- 	"<div><a href='https://fredlintz5.github.io/calculator/' target='_blank'>Calculator</a></div>",
- 	"<div><a href='https://fredlintz5.github.io/htmlCssPractice/qwerty/' target='_blank'>Jquery Qwerty Keyboard</a></div>",
- 	"<div><a href='https://fredlintz5.github.io/htmlCssPractice/hover/' target='_blank'>CSS Layout/Hover Fun</a></div>",
-	"<div><a href='https://fredlintz5.github.io/hangman-game/' target='_blank'>Hangman</a></div>"
+ 	"<div><a href='https://public.tableau.com/profile/daniel.bradley1709#!/vizhome/citi_bike_public/Story?publish=yes' target='_blank'>Tableau-Challenge</a></div>",
+ 	"<div><a href='https://github.com/87danielbradley/machine-learning-challenge' target='_blank'>Machine Learning Challenge WIP</a></div>",
+ 	"<div><a href='https://github.com/87danielbradley/Big-Data-Challenge' target='_blank'>CSS Layout/Big Data Challenge WIP</a></div>",
+	"<div><a href='https://github.com/87danielbradley/Data_Science_in_Biotechnology' target='_blank'>Data Science in Biotechnology</a></div>",
+	"<div><a href='https://github.com/87danielbradley/sql-challenge' target='_blank'>SQL Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/VBA-challenge/blob/master/VBAStocks/Stock%20Data/VBA_Macro.bas' target='_blank'>VBA Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/Excel-Challenge' target='_blank'>Excel Challenge</a></div>",
+	"<div><a href='https://github.com/87danielbradley/Python-Challenge' target='_blank'>Python Challenge</a></div>"
+
 	];
 	 
 
